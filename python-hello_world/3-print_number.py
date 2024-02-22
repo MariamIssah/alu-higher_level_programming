38ab6fd90c0f390c08db5bc9c6cc84821f201d7e
MariamIssah <m.issah1@alustudent.com>
2024-02-22 15:04:40 -0800

    3-print_number.py

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
-number = -5
-
-if isinstance(number, int) or isinstance(number, float):
-    if number > 0:
-        print(f"Positive: {number}")
-    elif number < 0:
-        print(f"Negative: {number}")
-    else:
-        print("Zero")
-else:
+number = 98
+print("{:d} Battery street".format(number))
