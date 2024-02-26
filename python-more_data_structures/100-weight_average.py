9aa3074c39fe6a2956c3280461c7be76f2889215
MariamIssah <m.issah1@alustudent.com>
2024-02-26 13:06:48 -0800

    100-weight_average.py

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 
+
+
 def weight_average(my_list=[]):
     if not my_list:  # Check if the list is empty
         return 0
-    
+
+
     total_weighted_sum = 0
     total_weight = 0
-    
+
+
     for score, weight in my_list:
         total_weighted_sum += score * weight
         total_weight += weight
-    
+
+
     return total_weighted_sum / total_weight
+
 
 
 # Test the function
