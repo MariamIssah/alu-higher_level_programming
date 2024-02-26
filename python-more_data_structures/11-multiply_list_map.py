5b64449ea95c3453d7932a72db1c204b69a22b11
MariamIssah <m.issah1@alustudent.com>
2024-02-26 12:11:04 -0800

    11-multiply_list_map.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 
-multiply_list = lambda lst, multiplier: list(map(lambda x: x * multiplier, lst))
+multiply_list_map = lambda lst, multiplier: list(map(lambda x: x * multiplier, lst))
