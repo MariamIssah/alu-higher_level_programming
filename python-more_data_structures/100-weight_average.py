0c309474b73fe45948a0dfa46f655f8f320d8a45
MariamIssah <m.issah1@alustudent.com>
2024-02-26 12:56:18 -0800

    100-weight_average.py

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -13,9 +13,9 @@ def weight_average(my_list=[]):
     
     return total_weighted_sum / total_weight
 
+
 # Test the function
 if __name__ == "__main__":
     my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
     result = weight_average(my_list)
     print("Average: {:0.2f}".format(result))
-
