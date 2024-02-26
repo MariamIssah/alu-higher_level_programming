d70ea9ad0bd543163db9c06f03e1c67040086b93
MariamIssah <m.issah1@alustudent.com>
2024-02-26 12:03:31 -0800

    11-multiply_list_map.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return (None)
 
-    return (max(a_dictionary, key=a_dictionary.get))
+def multiply_list(lst, multiplier):
+    return list(map(lambda x: x * multiplier, lst))
+
+# Test the function
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    multiplier = 10
+    result = multiply_list(my_list, multiplier)
+    print(result)
