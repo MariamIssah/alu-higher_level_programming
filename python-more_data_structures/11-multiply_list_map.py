ab4cabd16fa9504a7b72c13193a4fc566a272800
MariamIssah <m.issah1@alustudent.com>
2024-02-26 12:20:25 -0800

    11-multiply_list_map.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 
-def multiply_list_map(lst, multiplier):
-    """
-    Multiplies each element of the list by the multiplier using map function.
-    """
-    def multiply(x):
-        return x * multiplier
-    
-    return list(map(multiply, lst))
+multiply_list_map = lambda lst, multiplier: list(map(lambda x: x * multiplier,lst))
