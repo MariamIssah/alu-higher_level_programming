6b08003a315f5279132f5efb5b8c273065ed3a7f
MariamIssah <m.issah1@alustudent.com>
2024-02-26 05:49:49 -0800

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -9,4 +9,3 @@ def multiple_returns(sentence):
 # sentence = "At school, I learnt C!"
 # length, first = multiple_returns(sentence)
 # print("Length: {:d} - First character: {}".format(length, first))
-
