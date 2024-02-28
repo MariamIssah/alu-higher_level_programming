efe58967c293f3368356bdb9d857a4d486ce2cb6
MariamIssah <m.issah1@alustudent.com>
2024-02-27 22:54:31 -0800

    102-complex_delete.py

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,31 +1,14 @@
 #!/usr/bin/python3
+# 102-complex_delete.py
+# Brennan D Baraban <375@holbertonschool.com>
+
 
 def complex_delete(a_dictionary, value):
-    """
-    Deletes keys with a specific value in a dictionary.
-    """
-    if not a_dictionary:  # Check if dictionary is empty
-        return
-    
-    keys_to_delete = [key for key, val in a_dictionary.items() if val == value]
-    for key in keys_to_delete:
-        del a_dictionary[key]
+    """Delete keys with a specific value in a dictionary."""
+    while value in a_dictionary.values():
+        for k, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[k]
+                break
 
-# Tests
-if __name__ == "__main__":
-    # Test cases
-    test_cases = [
-        ({'a': "a", 'b': "b", 'c': "c", 'd': "d", 'e': "e"}, 'a'),
-        ({'a': "a", 'b': "b", 'c': "c", 'd': "d", 'e': "e"}, 'e'),
-        ({'a': "a", 'b': "b", 'c': "c", 'd': "d", 'e': "e"}, 'f'),
-        ({'a': "a", 'b': "b", 'c': "a", 'd': "a", 'e': "e"}, 'a'),
-        ({'c': "a", 'd': "a"}, 'a'),
-        ({}, 'a')
-    ]
-
-    for my_dict, value in test_cases:
-        print("Original Dictionary:", my_dict)
-        complex_delete(my_dict, value)
-        print("Dictionary after deletion:", my_dict)
-        print()
-
+    return (a_dictionary)
