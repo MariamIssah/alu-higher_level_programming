2d3dd92d225e6849b82cdb85511fc39ca1cd8e02
MariamIssah <m.issah1@alustudent.com>
2024-03-15 02:21:24 -0700

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Module 1-number_of_lines
-
-Contains function that returns number of lines in text"""
+Contains function that returns number of lines in text
+"""
 
 
 def write_file(filename="", text=""):
