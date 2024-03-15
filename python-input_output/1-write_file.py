d405bf9d240a33b1123974f7c0d83ef460f12056
MariamIssah <m.issah1@alustudent.com>
2024-03-15 02:09:06 -0700

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,14 +6,7 @@ Contains function that returns number of lines in text"""
 
 
 def write_file(filename="", text=""):
-    """
-    Function that writes to a text file.
-    Args:
-        filename: filename
-        text: text to write
-    Raises:
-        Exception: when the file can be opened
-    """
-
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
+        return len(text)
