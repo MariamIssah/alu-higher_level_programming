830b5decdcfcf554de6796647acc5de6ebbf7cab
MariamIssah <m.issah1@alustudent.com>
2024-03-17 02:55:23 -0700

    101-stats.py

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,57 +1,38 @@
 #!/usr/bin/python3
-""" Module to print status code """
 import sys
 
+def print_stats(total_size, status_codes):
+    print(f"File size: {total_size}")
+    sorted_codes = sorted(status_codes.keys())
+    for code in sorted_codes:
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
 
-class Magic:
-    """ Class to generates instances with dict and size"""
-    def __init__(self):
-        """ Init method """
-        self.dic = {}
-        self.size = 0
-
-    def init_dic(self):
-        """ Initialize dict """
-        self.dic['200'] = 0
-        self.dic['301'] = 0
-        self.dic['400'] = 0
-        self.dic['401'] = 0
-        self.dic['403'] = 0
-        self.dic['404'] = 0
-        self.dic['405'] = 0
-        self.dic['500'] = 0
-
-    def add_status_code(self, status):
-        """ add repeated number to the status code """
-        if status in self.dic:
-            self.dic[status] += 1
-
-    def print_info(self, sig=0, frame=0):
-        """ print status code """
-        print("File size: {:d}".format(self.size))
-        for key in sorted(self.dic.keys()):
-            if self.dic[key] is not 0:
-                print("{}: {:d}".format(key, self.dic[key]))
-
-
-if __name__ == "__main__":
-    magic = Magic()
-    magic.init_dic()
-    nlines = 0
+def main():
+    total_size = 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    line_count = 0
 
     try:
         for line in sys.stdin:
-            if nlines % 10 == 0 and nlines is not 0:
-                magic.print_info()
-
             try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                magic.add_status_code(list_line[-2])
-                magic.size += int(list_line[-1].strip("\n"))
-            except:
-                pass
-            nlines += 1
+                parts = line.split()
+                total_size += int(parts[-1])
+                status_code = int(parts[-2])
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+                line_count += 1
+
+                if line_count % 10 == 0:
+                    print_stats(total_size, status_codes)
+
+            except ValueError:
+                pass  # Ignore lines with incorrect format
+
     except KeyboardInterrupt:
-        magic.print_info()
+        print_stats(total_size, status_codes)
         raise
-    magic.print_info()
+
+if __name__ == "__main__":
+    main()
+
