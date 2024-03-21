d32c19630e0cf2bdb56cdab9ea97129a2df1ab55
MariamIssah <m.issah1@alustudent.com>
2024-03-21 02:16:39 -0700

    4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# sends specific header
-curl -s -H "X-HolbertonSchool-User-Id:98" "$1"
+
+# Check if URL argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send GET request with specific header using curl
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
