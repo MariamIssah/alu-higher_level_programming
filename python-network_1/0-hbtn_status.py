5680edac2ac31052b3b0975f05f3df911b86ae59
MariamIssah <m.issah1@alustudent.com>
2024-03-21 05:53:57 -0700

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that fetches https://alu-intranet.hbtn.io/status
-- using the urllib package.
-"""
-import urllib.request
+""" Fetches basic request with urlib"""
+from urllib import request
 
-
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+with request.urlopen("https://intranet.hbtn.io/status") as response:
+    r = response.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(r), r, r.decode('utf-8')))
