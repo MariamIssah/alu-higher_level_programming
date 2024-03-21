100827f1eabb2f851e47faac6061317748e35150
MariamIssah <m.issah1@alustudent.com>
2024-03-21 14:46:32 -0700

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that
-- fetches https://intranet.hbtn.io/status.
-"""
+"""Fetches https://intranet.hbtn.io/status."""
 import requests
 
+
 if __name__ == "__main__":
-    r = requests.get("https://alu-intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
