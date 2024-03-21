0afce02d747d5523b834a16e18bfca023d9c8bd9
MariamIssah <m.issah1@alustudent.com>
2024-03-21 15:12:58 -0700

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -7,10 +7,17 @@ import requests
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    r = requests.get(url, headers=headers)
-
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    
+    # Create a session object
+    with requests.Session() as session:
+        # Set User-Agent header
+        session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'})
+        
+        # Send GET request
+        r = session.get(url)
+        
+        # Display response
+        print("Body response:")
+        print("\t- type: {}".format(type(r.text)))
+        print("\t- content: {}".format(r.text))
 
