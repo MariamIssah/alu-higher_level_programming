33508c22a725b3e718623a659c8993c569427452
MariamIssah <m.issah1@alustudent.com>
2024-03-24 02:35:33 -0700

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status."""
 import urllib.request
 
+url = 'https://alu-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alu-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(<class'bytes'>)))
-        print("\t- content: {}".format(b'Custom status'))
-        print("\t- utf8 content: {}".format("Custom status")))
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+    utf8_content = content.decode('utf-8')
+
+print("Body response:")
+print("\t- type:", type(content))
+print("\t- content:", content)
+print("\t- utf8 content:", utf8_content)
