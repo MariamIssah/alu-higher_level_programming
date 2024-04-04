c28c029b0ec0771e1ff34becbe385c2f28685194
MariamIssah <m.issah1@alustudent.com>
2024-04-04 10:03:04 -0700

    model_state.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Lists all cities by state passed by user"""
+# Defines a State model.
+# Inherits from SQLAlchemy Base and links to the MySQL table states.
 
-import MySQLdb
-from sys import argv
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                           user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT cities.id, cities.name, states.name FROM cities
-        LEFT JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC;
-        """, (argv[4],))
-    query_rows = cur.fetchall()
-    print(", ".join([row[1] for row in query_rows]))
-    cur.close()
-    conn.close()
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents a state for a MySQL database.
+
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
