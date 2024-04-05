33992647c70bb5ec874833520530a84410652779
MariamIssah <m.issah1@alustudent.com>
2024-04-05 09:15:57 -0700

    100-matrix_mul.py

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -66,6 +66,3 @@ def matrix_mul(m_a, m_b):
         new_matrix.append(new_row)
 
     return new_matrix
-        except (TypeError, ValueError) as e:
-            print(f"Test Case {idx} failed: {e}")
-            print()
